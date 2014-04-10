3c44bda649240a4ab124ba0e67d36a176fc34de9
Florian Schuster <fgschuster@gmail.com>
2014-04-10 17:40:40 +0200

    Modify .gitignore & Add secure token algorithm

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SampleApp::Application.config.secret_key_base = 'df7de16f401b7defac613d57ed44d2fd9b04f0a045c82c4f529b3572929bd8dc632f03bd86fa158f13022068bac4cbaf6c6d7c221884a7bcb8f651e446c6dee7'
+require 'securerandom'
+
+def secure_token
+	token_file = Rails.root.join('.secret')
+	if File.exist?(token_file)
+		# Use the exisitng token
+		File.read(token_file).chomp
+	else
+		# Generate a new token and store it in token_file
+		token = SecureRandom.hex(64)
+		File.write(token_file, token)
+		token
+	end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,7 +8,7 @@ describe "Static pages" do
 			page.should have_selector('h1', :text => 'Sample App')
 		end
 		it "should have the right title" do
-			visit 'static_pages/home'
+			visit '/static_pages/home'
 			page.should have_selector('title', 
 				:text => 'Ruby on Rails Tutorial Sample App | Home')
 		end	
@@ -20,7 +20,7 @@ describe "Static pages" do
 			page.should have_selector('h1', :text => 'Help')
 		end
 		it "should have the right title" do
-			visit 'static_pages/help'
+			visit '/static_pages/help'
 			page.should have_selector('title', 
 				:text => "Ruby on Rails Tutorial Sample App | Help")
 		end	
@@ -32,7 +32,7 @@ describe "Static pages" do
 			page.should have_selector('h1', :text => 'About Us')
 		end
 		it "should have the right title" do
-			visit 'static_pages/about'
+			visit '/static_pages/about'
 			page.should have_selector('title', 
 				:text => "Ruby on Rails Tutorial Sample App | About Us")
 		end	
